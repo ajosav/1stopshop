69493c7196c933b4c5a89037dc365d7977d0484d
Joshua Adebayo <jadebayo@renmoney.com>
2021-04-20 20:41:15 +0100

    fixed conflict in partdealer model

--- app/Http/Middleware/LogRequest.php
+++ app/Http/Middleware/LogRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use App\Models\LogApiCall;
 use Closure;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 
 class LogRequest
@@ -19,20 +20,25 @@ class LogRequest
     {
         $response = $next($request);
 
-        $data = [
-            'type'              => 'request',
-            'status'            => $response->getStatusCode(),
-            'tag'               => auth()->user() ? auth()->user()->email : 'guest',
-            'uri'               => str_replace($request->root(), '', $request->fullUrl()) ?: '/',
-            'method'            => $request->method() ?? null,
-            'controller_action' => $request->route() ? $request->route()->getActionName() : null,
-            'middleware'        => json_encode(array_values($request->route() ? $request->route()->gatherMiddleware() : [])),
-            'payload'           => json_encode($request->except('password', 'password_confirmation')),
-            'duration'          => now()->parse($request->server('REQUEST_TIME'))->fromNow(null, false, 3),
-            // 'response'          => $response->getContent() ? json_encode($response->getContent()) : null
-        ];
-        LogApiCall::create($data);
-        // Log::debug("{$response->getStatusCode()} - Log activity for {$request->method()} {$request->getRequestUri()}", $data);
+        try {
+       
+            $data = [
+                'type'              => 'request',
+                'status'            => $response->getStatusCode(),
+                'tag'               => auth()->user() ? auth()->user()->email : 'guest',
+                'uri'               => str_replace($request->root(), '', $request->fullUrl()) ?: '/',
+                'method'            => $request->method() ?? null,
+                'controller_action' => $request->route() ? $request->route()->getActionName() : null,
+                'middleware'        => json_encode(array_values($request->route() ? $request->route()->gatherMiddleware() : [])),
+                'payload'           => json_encode($request->except('password', 'password_confirmation')),
+                'duration'          => now()->parse($request->server('REQUEST_TIME'))->fromNow(null, false, 3),
+                // 'response'          => $response->getContent() ? json_encode($response->getContent()) : null
+            ];
+            LogApiCall::create($data);
+             
+        } catch(QueryException $e) {
+            report($e);
+        }
 
         return $response;
     }
