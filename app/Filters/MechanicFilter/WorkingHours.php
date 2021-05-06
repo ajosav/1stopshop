59f7d24ef2cdbb6e312f46719b08d02ea4cbb983
Joshua Adebayo <jadebayo@renmoney.com>
2021-05-07 00:29:27 +0100

    filter mechanics by working hours

--- app/Filters/MechanicFilter/WorkingHours.php
+++ app/Filters/MechanicFilter/WorkingHours.php
@@ -13,27 +13,60 @@ class WorkingHours extends BaseFilter {
                 if($time_of_avail == 'morning') {
                     return $working_hour->where('day', date("l"))
                     ->where(function($time) {
-                        return $time->where('from_hour', '<=', 5)
-                        ->orWhere('to_hour', '>', 12);
+                        return $time->where(function($query) {
+                            return $query->whereBetween('from_hour', [5, 12])
+                                        ->where('from_meridian', 'AM');
+                            })->orWhere(function($query) {
+                                return $query->whereBetween('to_hour', [5, 12])
+                                            ->where('to_meridian', 'AM');
+
+                            });
+                        // return $time->where('from_hour', '<=', 5)
+                        // ->orWhere('to_hour', '>', 12);
                     });
                 } elseif($time_of_avail == 'afternoon') {
                     return $working_hour->where('day', date("l"))
                     ->where(function($time) {
-                        return $time->where('from_hour', '<=', 12)
-                            ->orWhere('to_hour', '>', 18);
+                        return $time->where(function($query) {
+                                return $query->whereBetween('from_hour', [12, 24])
+                                            ->where('from_meridian', 'PM');
+                                })
+                            ->orWhere(function($query) {
+                                return $query->whereBetween('to_hour', [12, 24])
+                                            ->where('to_meridian', 'PM');
+
+                            });
+                        // return $time->where('from_hour', '<=', 12)
+                        //     ->orWhere('to_hour', '>', 18);
                     });
                 } elseif($time_of_avail == 'evening') {
                     return $working_hour->where('day', date("l"))
                     ->where(function($time) {
-                        return $time->where('from_hour', '<=', 18)
-                            ->orWhere('to_hour', '>', 24);
+                        return $time->where(function($query) {
+                            return $query->whereBetween('from_hour', [18, 24])
+                                        ->where('from_meridian', 'PM');
+                            })->orWhere(function($query) {
+                                return $query->whereBetween('to_hour', [18, 24])
+                                            ->where('to_meridian', 'PM');
+
+                            });
+                        // return $time->where('from_hour', '<=', 18)
+                        //     ->orWhere('to_hour', '>', 24);
                     });
-                } elseif($time_of_avail == 'night') {
+                } elseif($time_of_avail == 'early_morning') {
                     return $working_hour->where('day', date("l"))
-                    ->where(function($time) {
-                        return $time->where('from_hour', '<=', 00)
-                            ->orWhere('to_hour', '>', 5);
-                    });
+                        ->where(function($time) {
+                        return $time->where(function($query) {
+                            return $query->whereBetween('from_hour', ['00', '5'])
+                                        ->where('from_meridian', 'AM');
+                            })->orWhere(function($query) {
+                                return $query->whereBetween('to_hour', ['00', '5'])
+                                            ->where('to_meridian', 'AM');
+
+                            });
+                            // return $time->where('from_hour', '<=', 00);
+                                // ->orWhere('to_hour', '>', 5);
+                        });
                 }
 
             });
