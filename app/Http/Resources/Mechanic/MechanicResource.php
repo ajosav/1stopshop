c26f701eec56245f0b4c739b0da88a7919123051
Joshua Adebayo <jadebayo@renmoney.com>
2021-05-19 12:04:10 +0100

    Updated work hours data

--- app/Http/Resources/Mechanic/MechanicResource.php
+++ app/Http/Resources/Mechanic/MechanicResource.php
@@ -31,7 +31,7 @@ class MechanicResource extends JsonResource
             'city'                          =>          $this->city,
             'company_photo'                 =>          $this->company_photo,
             'working_hours'                 =>          $this->working_hours,
-            'working_hours_data'            =>          $this->work_hours_data,
+            'work_hours_data'            =>          $this->work_hours_data,
             'customer_reviews'              =>          $this->customerReviews(),
         ];
     }
