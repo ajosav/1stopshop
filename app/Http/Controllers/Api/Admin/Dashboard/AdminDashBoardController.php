64dcc427756aea31111d466c0e762497dbd85388
Joshua Adebayo <jadebayo@renmoney.com>
2021-06-28 16:34:30 +0100

    added dashboard analytics endpoint

--- app/Http/Controllers/Api/Admin/Dashboard/AdminDashBoardController.php
+++ app/Http/Controllers/Api/Admin/Dashboard/AdminDashBoardController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\Api\Admin\Dashboard;
 
-use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\User\UserResourceCollection;
+use Illuminate\Support\Facades\Cache;
 
 class AdminDashBoardController extends Controller
 {
@@ -12,5 +14,40 @@ class AdminDashBoardController extends Controller
         $total_users = User::whereBetween('created_at', [now()->startOfWeek(), now()->endOfWeek()])->get();
 
         return $total_users;
+    }
+
+    public function registeredUsers() {
+        $daily_users = User::with(['mechanic', 'partDealer'])->whereDate('created_at', now())->get();
+
+        return UserResourceCollection::collection($daily_users)->additional([
+            'message' => 'Daily users retrieved successfully',
+            'status' => "success"
+        ]);
+
+        // whereDate('created_at', now())->get();
+        // $daily_users = User::leftJoin('mechanics', 'users.id', '=', 'mechanics.user_id')
+        //                     ->leftJoin('part_dealers', 'users.id', '=', 'part_dealers.user_id')
+        //                     ->orderBy('users.created_at', 'desc')
+        //                     ->get();
+
+    }
+
+    public function getUsersByRoleCount() {
+        $mechanics      = User::join('mechanics', 'users.id', '=', 'mechanics.user_id')->count();
+        $part_dealers   = User::join('part_dealers', 'users.id', '=', 'part_dealers.user_id')->count();
+        $users_count    = User::count();
+        $regular_user   = User::doesntHave('mechanic')->doesntHave('partDealer')->count();
+
+        return response()->success("Successfully returned users count", [
+            'mechanics_count' => $mechanics,
+            'part_dealers_count' => $part_dealers,
+            'regular_user_count' => $regular_user,
+            'total_users' => $users_count
+        ]);
+
+
+        //                     ->leftJoin('part_dealers', 'users.id', '=', 'part_dealers.user_id')
+        //                     ->orderBy('users.created_at', 'desc')
+        //                     ->get();
     }
 }
--- routes/admin-api.php
+++ routes/admin-api.php
@@ -12,7 +12,9 @@ Route::group(['prefix' => 'admin', 'name' => 'admin'], function () {
     Route::get('logout', [AuthController::class, 'logout'])->name('admin');
 
     
-    Route::middleware('auth.jwt:admin')->group(function () {
+    // Route::middleware('auth.jwt:admin')->group(function () {
         Route::get('registered', [AdminDashBoardController::class, 'analytics'])->name('registered');
-    });
+        Route::get('daily-registered-users', [AdminDashBoardController::class, 'registeredUsers'])->name('daily-registered-users');
+        Route::get('count-registered-users', [AdminDashBoardController::class, 'getUsersByRoleCount'])->name('count-registered-users');
+    // });
 });
